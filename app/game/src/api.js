bf89e7d4b332998056ccac8177f1a4499fcc66c4
porridgewithraisins <santhanambr2002@gmail.com>
2023-05-18 13:04:51 +0530

    forgot to export

--- app/game/src/api.js
+++ app/game/src/api.js
@@ -1,10 +1,10 @@
-function getQuestion() {
+export function getQuestion() {
     const jwt = localStorage.getItem("jwt");
     const url = "https://backend-paradox.ieeecsvitc.com/question";
     return fetch(url, { headers: { Authorization: `Bearer ${jwt}` } }).then(res => res.json());
 }
 
-function postAnswer(answer) {
+export function postAnswer(answer) {
     const jwt = localStorage.getItem("jwt");
     const url = "https://backend-paradox.ieeecsvitc.com/answer";
     return fetch(url, {
@@ -14,7 +14,7 @@ function postAnswer(answer) {
     }).then(res => res.json());
 }
 
-function getUserData() {
+export function getUserData() {
     const jwt = localStorage.getItem("jwt");
     const url = "https://backend-paradox.ieeecsvitc.com/me";
     return fetch(url, { headers: { Authorization: `Bearer ${jwt}` } }).then(res => res.json());
