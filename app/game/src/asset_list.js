969b280164df21f509c48bbf0e979fd9f4a94647
porridgewithraisins <santhanambr2002@gmail.com>
2023-05-20 20:50:53 +0530

    add levels

--- app/game/src/asset_list.js
+++ app/game/src/asset_list.js
@@ -1,82 +1,102 @@
 export const ASSET_LIST = [
     {
-        "name": "ss_12_fbking_walkL.png",
-        "url": "assets/img/ss_12_fbking_walkL.png",
-        "type": "img"
+        name: "ss_12_fbking_walkL.png",
+        url: "assets/img/ss_12_fbking_walkL.png",
+        type: "img",
     },
     {
-        "name": "ss_12_fbking_walkR.png",
-        "url": "assets/img/ss_12_fbking_walkR.png",
-        "type": "img"
+        name: "ss_12_fbking_walkR.png",
+        url: "assets/img/ss_12_fbking_walkR.png",
+        type: "img",
     },
     {
-        "name": "ss_1_fbking_idleL.png",
-        "url": "assets/img/ss_1_fbking_idleL.png",
-        "type": "img"
+        name: "ss_1_fbking_idleL.png",
+        url: "assets/img/ss_1_fbking_idleL.png",
+        type: "img",
     },
     {
-        "name": "ss_1_fbking_idleR.png",
-        "url": "assets/img/ss_1_fbking_idleR.png",
-        "type": "img"
+        name: "ss_1_fbking_idleR.png",
+        url: "assets/img/ss_1_fbking_idleR.png",
+        type: "img",
     },
     {
-        "name": "ss_1_coin.png",
-        "url": "assets/img/ss_1_coin.png",
-        "type": "img"
+        name: "ss_1_coin.png",
+        url: "assets/img/ss_1_coin.png",
+        type: "img",
     },
     {
-        "name": "ss_1_chest_open.png",
-        "url": "assets/img/ss_1_chest_open.png",
-        "type": "img"
+        name: "ss_1_chest_open.png",
+        url: "assets/img/ss_1_chest_open.png",
+        type: "img",
     },
     {
-        "name": "ss_1_chest_closed.png",
-        "url": "assets/img/ss_1_chest_closed.png",
-        "type": "img"
+        name: "ss_1_chest_closed.png",
+        url: "assets/img/ss_1_chest_closed.png",
+        type: "img",
     },
     {
-        "name": "ss_1_chest_closed.png",
-        "url": "assets/img/ss_1_chest_closed.png",
-        "type": "img"
+        name: "ss_1_chest_closed.png",
+        url: "assets/img/ss_1_chest_closed.png",
+        type: "img",
     },
     {
-        "name": "ss_1_chest_inaccessible.png",
-        "url": "assets/img/ss_1_chest_inaccessible.png",
-        "type": "img"
+        name: "ss_1_chest_inaccessible.png",
+        url: "assets/img/ss_1_chest_inaccessible.png",
+        type: "img",
     },
     {
-        "name": "ss_1_door.png",
-        "url": "assets/img/ss_1_door.png",
-        "type": "img"
+        name: "ss_1_door.png",
+        url: "assets/img/ss_1_door.png",
+        type: "img",
     },
     {
-        "name": "ss_1_ladderseg.png",
-        "url": "assets/img/ss_1_ladderseg.png",
-        "type": "img"
+        name: "ss_1_ladderseg.png",
+        url: "assets/img/ss_1_ladderseg.png",
+        type: "img",
     },
     {
-        "name": "ss_1_platform.png",
-        "url": "assets/img/ss_1_platform.png",
-        "type": "img"
+        name: "ss_1_platform.png",
+        url: "assets/img/ss_1_platform.png",
+        type: "img",
     },
     {
-        "name": "ss_1_platformedge.png",
-        "url": "assets/img/ss_1_platformedge.png",
-        "type": "img"
+        name: "ss_1_platformedge.png",
+        url: "assets/img/ss_1_platformedge.png",
+        type: "img",
     },
     {
-        "name": "ss_1_platformslim.png",
-        "url": "assets/img/ss_1_platformslim.png",
-        "type": "img"
+        name: "ss_1_platformslim.png",
+        url: "assets/img/ss_1_platformslim.png",
+        type: "img",
     },
     {
-        "name": "scene1.json",
-        "url": "assets/text/scene1.json",
-        "type": "text"
+        name: "scene1.json",
+        url: "assets/text/scene1.json",
+        type: "text",
     },
     {
-        "name": "background.jpg",
-        "url": "assets/img/background.jpg",
-        "type": "img"
+        name: "scene2.json",
+        url: "assets/text/scene2.json",
+        type: "text",
+    },
+    {
+        name: "scene3.json",
+        url: "assets/text/scene3.json",
+        type: "text",
+    },
+    {
+        name: "scene4.json",
+        url: "assets/text/scene4.json",
+        type: "text",
+    },
+    {
+        name: "scene5.json",
+        url: "assets/text/scene5.json",
+        type: "text",
+    },
+    {
+        name: "background.jpg",
+        url: "assets/img/background.jpg",
+        type: "img",
     },
 ];
