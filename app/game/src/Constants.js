c97bbb16474f03671e49577b3ba9f3df473c35e5
porridgewithraisins <santhanambr2002@gmail.com>
2023-05-20 22:36:40 +0530

    catch cancel error

--- app/game/src/Constants.js
+++ app/game/src/Constants.js
@@ -183,7 +183,9 @@ export const Game = {
             ({ level, text, image, raw } = await getQuestion());
             await genericChecks(raw);
 
-            const answer = await input({ text: `Level ${level}: ${text}`, imgUrl: image });
+            const answer = await input({ text: `Level ${level}: ${text}`, imgUrl: image }).catch(
+                noop => noop
+            );
             ({ correct, raw, error } = await postAnswer(answer));
             await genericChecks(raw, "submitting your answer");
 
